6a507a367a804e391abdc94ea3848547389aff6a
tomoki_sun <tomoki69386@gmail.com>
2020-02-03 14:36:19 +0900

    Add list of avatar

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,18 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class Issue {
+  Issue({
+    this.title,
+    this.avatarUrl,
+  });
+
+  final String title;
+  final String avatarUrl;
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _titles = <String>[];
+  List<Issue> _issues = <Issue>[];
 
   @override
   void initState() {
@@ -61,7 +71,10 @@ class _MyHomePageState extends State<MyHomePage> {
       final issues = data as List;
       issues.forEach((dynamic element) {
         final issue = element as Map;
-        _titles.add(issue['title'] as String);
+        _issues.add(Issue(
+          title: issue['title'] as String,
+          avatarUrl: issue['user']['avatar_url'] as String,
+        ));
       });
     });
   }
@@ -74,12 +87,16 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          if (index >= _titles.length) {
+          if (index >= _issues.length) {
             return null;
           }
 
+          final issue = _issues[index];
           return ListTile(
-            title: Text(_titles[index]),
+            leading: ClipOval(
+              child: Image.network(issue.avatarUrl),
+            ),
+            title: Text(issue.title),
           );
         },
       ),
