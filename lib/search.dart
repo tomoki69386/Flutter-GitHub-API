2c71a4469b021f512ebd0997ab5fca9bddcdc63d
tomoki_sun <tomoki69386@gmail.com>
2020-02-03 17:44:13 +0900

    Add GitHub Search

--- lib/search.dart
+++ lib/search.dart
@@ -4,6 +4,16 @@ import 'package:example/detail.dart';
 import 'package:http/http.dart' as http;
 import 'package:loader_search_bar/loader_search_bar.dart';
 
+class User {
+  User({
+    this.login,
+    this.avatarUrl,
+  });
+
+  final String login;
+  final String avatarUrl;
+}
+
 class SearchPage extends StatefulWidget {
   SearchPage({Key key, this.title}) : super(key: key);
 
@@ -15,6 +25,9 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
 
+  List<User> _items = <User>[];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SearchBar(
@@ -22,9 +35,42 @@ class _SearchPageState extends State<SearchPage> {
           title: Text("Search"),
         ),
         onQueryChanged: (query) {
-          print(query);
+          _load(query);
         },
       ),
+      body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            if (index >= _items.length) {
+              return null;
+            }
+            final item = _items[index];
+            return ListTile(
+              leading: ClipOval(
+                child: Image.network(item.avatarUrl),
+              ),
+              title: Text(item.login),
+              onTap: () {
+                // 遷移させる
+              },
+            );
+          }),
     );
+  }
+
+  Future<void> _load(String query) async {
+    final res = await http.get("https://api.github.com/search/users?q=" + query);
+    final data = json.decode(res.body);
+    setState(() {
+      final items = data["items"] as List;
+      final List<User> elements = <User>[];
+      items.forEach((dynamic element) {
+        final item = element as Map;
+        elements.add(User(
+          login: item['login'] as String,
+          avatarUrl: item['avatar_url'] as String,
+        ));
+      });
+      _items = elements;
+    });
   }
 }
